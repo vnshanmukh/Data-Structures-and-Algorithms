e7b0f9641118850839187d818b3f56c142daf211
vnshanmukh <67438038+vnshanmukh@users.noreply.github.com>
2021-07-04 12:56:57 +0530

    Delete largest_sum_array.py

--- largest_sum_array.py
+++
@@ -1,8 +0,0 @@
-def maxSubArraySum(a):
-    max_so_far = a[0]
-    curr_max = a[0]
-    for i in range(1,len(a)):
-        curr_max = max(a[i], curr_max + a[i])
-        max_so_far = max(max_so_far,curr_max)
-    return max_so_far
-print(maxSubArraySum([1, 2, 3, -4, 5])) 
